a492e1be343a9b29730956ffada87bf07964698d
elizailtnere <149582165+elizailtnere@users.noreply.github.com>
2024-09-11 15:59:05 +0300

    Uzdevumi11.09.

---
+++ uzd.py
@@ -0,0 +1,11 @@
+""""
+Elīza Iltnere 
+11.09.24 
+Izveidot programmu, kas aprēķina pirkumu bez pvn un pašu pvn vērtību.
+
+"""
+
+pvn = 0.79
+nauda = float(input('Ievadi savu summu: '))
+vert = nauda*pvn
+print(vert)
---
+++ uzd2.py
@@ -0,0 +1,21 @@
+""""
+Elīza Iltnere 
+11.09.24 
+Izveidot programmu, kas  lietotājs secīgi var pabeigt 5 teikumus.
+
+"""
+
+username = input("Mani sauc:")
+print("Mani sauc", username)
+
+username1 = input("Man patīk:")
+print("Man patīk", username1)
+
+username2 = input("Man ir:")
+print("Man ir", username2 , 'gadi')
+
+username3 = input("Es mīlu:")
+print("Es mīlu", username3)
+
+username4 = input("Mana lielākā vēlēšanās:")
+print("Mana lielākā vēlēšanas ir", username4)
